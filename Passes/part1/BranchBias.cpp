3907ca3b4c687a7eaecb06e56d62c84b738da152
范志康 <goodman.capu@gmail.com>
2018-01-27 22:07:15 -0800

    P1S3: BranchBias

--- Passes/part1/BranchBias.cpp
+++ Passes/part1/BranchBias.cpp
@@ -1,8 +1,11 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/raw_ostream.h"
+#include <vector>
 
 using namespace llvm;
+using namespace std;
 
 namespace {
     struct BranchBias : public FunctionPass {
@@ -10,6 +13,34 @@ namespace {
         BranchBias() : FunctionPass(ID) {}
         
         bool runOnFunction(Function &F) override {
+            // Get module and context
+            Module *mod = F.getParent();
+            LLVMContext &ctxt = mod->getContext();
+            
+            // Get handle of function
+            Function *updateFunc = cast<Function>(mod->getOrInsertFunction("updateBranchInfo", Type::getVoidTy(ctxt), Type::getInt1Ty(ctxt)));
+            Function *printFunc = cast<Function>(mod->getOrInsertFunction("printOutBranchInfo", Type::getVoidTy(ctxt)));
+            
+            for (BasicBlock &BB : F) {
+                // Get builder
+                IRBuilder<> Builder(&BB);
+                
+                // Insert update function
+                BranchInst *terminator = dyn_cast<BranchInst>(BB.getTerminator());
+                if (terminator && terminator->isConditional()) {
+                    vector<Value *> args = {terminator->getCondition()};
+                    Builder.SetInsertPoint(BB.getTerminator());
+                    Builder.CreateCall(updateFunc, args);
+                }
+                
+                // Insert print function
+                for (Instruction &I : BB) {
+                    if (I.getOpcode() == 1) { // "ret"
+                        Builder.SetInsertPoint(&I);
+                        Builder.CreateCall(printFunc);
+                    }
+                }
+            }
             
             return false; // Function is not modified
         }
